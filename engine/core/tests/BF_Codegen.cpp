c51eb05208827236775cca5a2e3192f97ded9a85
Geert S. <101360451+Iso83@users.noreply.github.com>
2026-04-16 14:32:08 +0200

    Align emitter semantics with constant evaluator

--- engine/core/tests/BF_Codegen.cpp
+++ engine/core/tests/BF_Codegen.cpp
@@ -1,4 +1,5 @@
 #include <BitFlow/core/codegen/Emitter.h>
+#include <BitFlow/core/eval/ConstantEval.h>
 
 #include <tests/common/Core_Expr.h>
 #include <tests/common/TestAssert.h>
@@ -10,10 +11,30 @@ int main() {
     auto a = MakeVar(1);
     auto b = MakeVar(2);
 
-    auto expr = MakeOp(10, OpType::Add, {a, b});
+    auto addExpr = MakeOp(10, OpType::Add, {a, b});
+    auto addCode = Codegen::EmitCExpr(addExpr, 32);
 
-    auto code = Codegen::EmitCExpr(expr, 32);
+    BF_TEST(addCode.find("+") != std::string::npos);
+    BF_TEST(addCode.find("((1ull << 32) - 1ull)") != std::string::npos);
 
-    BF_TEST(code == "((v1 + v2)) & ((1ull << 32) - 1)");
+    auto c1 = MakeConst(11, 1);
+    auto c40 = MakeConst(12, 40);
+    auto shlExpr = MakeOp(13, OpType::Shl, {c1, c40});
+
+    auto shlEval = Eval::EvaluateConstant(shlExpr, 32);
+    BF_TEST(shlEval.status == Eval::EvalStatus::Success);
+    BF_TEST(shlEval.value == (1ull << (40ull % 32ull)));
+
+    auto shlCode = Codegen::EmitCExpr(shlExpr, 32);
+    BF_TEST(shlCode.find("% 32ull") != std::string::npos);
+
+    auto rotExpr = MakeOp(14, OpType::RotL, {MakeConst(15, 0x12u), MakeConst(16, 33u)});
+    auto rotEval = Eval::EvaluateConstant(rotExpr, 32);
+    BF_TEST(rotEval.status == Eval::EvalStatus::Success);
+
+    auto rotCode = Codegen::EmitCExpr(rotExpr, 32);
+    BF_TEST(rotCode.find("% 32ull") != std::string::npos);
+    BF_TEST(rotCode.find("((1ull << 32) - 1ull)") != std::string::npos);
+
     return 0;
 }
