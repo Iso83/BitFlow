246bbf21dcc3f3b3d48d3bc9616339a369a73de7
Geert S. <101360451+Iso83@users.noreply.github.com>
2026-04-18 11:34:19 +0200

    core: add base bf_uint shift operators

--- engine/core/src/bitvector/BitVector.cpp
+++ engine/core/src/bitvector/BitVector.cpp
@@ -66,4 +66,63 @@ bf_uint bf_uint::operator~() const {
     return r;
 }
 
+bf_uint bf_uint::Shl(uint32_t s) const {
+    if (m_bw == 0)
+        return bf_uint(0);
+
+    s %= m_bw;
+
+    bf_uint r(m_bw);
+
+    const uint32_t wordShift = s / 64;
+    const uint32_t bitShift = s % 64;
+
+    for (int i = static_cast<int>(m_words.size()) - 1; i >= 0; --i) {
+        uint64_t val = 0;
+
+        if (i - static_cast<int>(wordShift) >= 0) {
+            val = m_words[static_cast<size_t>(i - static_cast<int>(wordShift))] << bitShift;
+
+            if (bitShift && i - static_cast<int>(wordShift) - 1 >= 0) {
+                val |= m_words[static_cast<size_t>(i - static_cast<int>(wordShift) - 1)] >>
+                       (64 - bitShift);
+            }
+        }
+
+        r.m_words[static_cast<size_t>(i)] = val;
+    }
+
+    r.Normalize();
+    return r;
+}
+
+bf_uint bf_uint::Shr(uint32_t s) const {
+    if (m_bw == 0)
+        return bf_uint(0);
+
+    s %= m_bw;
+
+    bf_uint r(m_bw);
+
+    const uint32_t wordShift = s / 64;
+    const uint32_t bitShift = s % 64;
+
+    for (size_t i = 0; i < m_words.size(); ++i) {
+        uint64_t val = 0;
+        const size_t src = i + wordShift;
+
+        if (src < m_words.size()) {
+            val = m_words[src] >> bitShift;
+            if (bitShift && (src + 1) < m_words.size()) {
+                val |= m_words[src + 1] << (64 - bitShift);
+            }
+        }
+
+        r.m_words[i] = val;
+    }
+
+    r.Normalize();
+    return r;
+}
+
 } // namespace BitFlow::Core::BitVector
