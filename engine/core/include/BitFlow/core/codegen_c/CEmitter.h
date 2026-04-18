6e8f3368bd761017f281010e6895c7bb3a532f23
Geert S. <101360451+Iso83@users.noreply.github.com>
2026-04-18 02:03:36 +0200

    Align CEmitter API with Step 15.2 declaration

--- engine/core/include/BitFlow/core/codegen_c/CEmitter.h
+++ engine/core/include/BitFlow/core/codegen_c/CEmitter.h
@@ -1,41 +1,14 @@
 #pragma once
 
 #include <cstdint>
-#include <map>
 #include <string>
-#include <vector>
 
 namespace BitFlow::Core::AST {
 struct Expr;
 }
 
-namespace BitFlow::Core::CodegenC {
+namespace BitFlow::Core::Codegen {
 
-struct CParameter {
-    uint32_t id = 0;
-    std::string type;
-    std::string name;
-};
+std::string EmitCFunction(const AST::Expr* root, uint32_t bitWidth);
 
-struct CLocal {
-    std::string type;
-    std::string name;
-    std::string expr;
-};
-
-struct CFunction {
-    std::string returnType;
-    std::string functionName;
-    std::vector<CParameter> parameters;
-    std::vector<CLocal> locals;
-    std::string returnExpr;
-};
-
-std::vector<CParameter> BuildCParameters(const AST::Expr* root,
-                                         const std::map<uint32_t, std::string>& names = {});
-std::vector<CLocal> BuildCLocals(const AST::Expr* root, uint32_t bitWidth);
-CFunction BuildCFunction(const AST::Expr* root, uint32_t bitWidth, const std::string& functionName = "f",
-                         const std::map<uint32_t, std::string>& names = {});
-std::string EmitCFunction(const CFunction& function);
-
-} // namespace BitFlow::Core::CodegenC
+} // namespace BitFlow::Core::Codegen
